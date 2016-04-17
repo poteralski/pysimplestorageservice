a6c27e321e71d4bf28fdb32aa3f6d96baac69e71
ppoteralski <poteralski.dev@gmail.com>
2016-04-17 14:01:19 +0200

    refactor

--- pysimplestorageservice/core.py
+++ pysimplestorageservice/core.py
@@ -41,7 +41,7 @@ class AmazonAWSManager(object):
         endpoint = self.__build_endpoint(bucket)
         r = requests.get(endpoint, headers=headers, params=params)
         if r.status_code == 200:
-            return self.parse_xml(r.content)
+            return r.content
         else:
             return None
 
@@ -127,112 +127,11 @@ class AmazonAWSManager(object):
         else:
             return endpoint
 
-    def parse_xml(self, xml_str):
-        files = []
-        import xml.etree.ElementTree as ET
-        import re
-        root = ET.fromstring(xml_str)
-        # cleaning tags...
-        for child in root:
-            child.tag = re.sub('{.*}', '', child.tag)
-        # listing filenames
-        for file in root.findall('Contents'):
-            files.append(file[0].text)
-        if root.findall('Prefix')[0].text.split("/")[0] == "log_uploads":
-            common_prefixes = root.findall('CommonPrefixes')
-            if len(common_prefixes) > 0:
-                for prefix in common_prefixes[0]:
-                    root = self.get_file_list(bucket="stava-applogs",
-                                              prefix=prefix.text)
-                    return root
-        return files
-
-    def parse_xml_for_dirs(self, xml_str):
-        files = []
-        import xml.etree.ElementTree as ET
-        import re
-        root = ET.fromstring(xml_str)
-        # cleaning tags...
-        for child in root:
-            child.tag = re.sub('{.*}', '', child.tag)
-        # listing filenames
-        for file in root.findall('Contents'):
-            files.append(file[0].text)
-        if root.findall('Prefix')[0].text.split("/")[0] == "log_uploads":
-            common_prefixes = root.findall('CommonPrefixes')
-            if len(common_prefixes) > 0:
-                dirs = []
-                for common_prefix in common_prefixes:
-                    for prefix in common_prefix:
-                        dirs.append(prefix.text.split("/")[-2])
-                return dirs
-        return files
-
-    def dir_ls(self, list, dir_name):
-        filenames = []
-        for l in list:
-            if dir_name in l:
-                filenames.append(l.split("/")[-1])
-        return filenames
-
-    def dir_ls_applogs(self, list, dir_name):
-        filenames = []
-        for l in list:
-            if dir_name in l:
-                filenames.append(l.split("/")[-2]+"/"+l.split("/")[-1])
-        return filenames
-
     def build_string_to_sign(self, algorithm, amz_date, canonical_request, credential_scope):
         return algorithm + '\n' + \
                amz_date + '\n' + \
                credential_scope + '\n' + \
                self.build_payload_hash(canonical_request)
-
-    def get_dir_list(self, bucket=None, prefix=None, max_keys=None):
-        t = get_utc_now()
-        method = 'GET'
-        amz_date = t.strftime('%Y%m%dT%H%M%SZ')
-        date_stamp = t.strftime('%Y%m%d')
-        host = bucket + '.s3.amazonaws.com'
-        endpoint = "http://" + host
-        service = 's3'
-        region = 'eu-central-1'
-
-        canonical_uri = '/'
-        canonical_querystring = 'delimiter='+urllib2.quote('/',safe='')
-        if max_keys is not None:
-            canonical_querystring = canonical_querystring + '&max-keys=' + urllib2.quote(str(max_keys),safe='')
-        if prefix is not None:
-            canonical_querystring = canonical_querystring + '&prefix=' + urllib2.quote(prefix,safe='')
-        payload_hash = self.build_payload_hash("")
-        canonical_headers = 'host:' + host + '\n' + \
-                            'x-amz-content-sha256:' + payload_hash + '\n' + \
-                            'x-amz-date:' + amz_date + '\n'
-        signed_headers = 'host;x-amz-content-sha256;x-amz-date'
-        canonical_request = self.build_cannonical_request(canonical_headers, canonical_querystring, canonical_uri,
-                                                          method, payload_hash, signed_headers)
-        #print canonical_request
-        algorithm = 'AWS4-HMAC-SHA256'
-        credential_scope = self.build_credential_scope(date_stamp, region, service)
-        string_to_sign = self.build_string_to_sign(algorithm, amz_date, canonical_request, credential_scope)
-        signing_key = self.get_signature_key(self.secret_key, date_stamp,
-                                           region, service)
-        signature = self.build_signature(signing_key, string_to_sign)
-        authorization_header = algorithm + ' ' + 'Credential=' + self.access_key + '/' + credential_scope + ',' + \
-                               'SignedHeaders=' + signed_headers + ',' + 'Signature=' + signature
-        #print authorization_header
-
-        headers = {
-            'Authorization': authorization_header,
-            'Date': t.strftime("%A, %d %B %Y %H:%M:%S"),
-            'x-amz-content-sha256': payload_hash,
-            'x-amz-date': amz_date
-            }
-        r = requests.get(endpoint+"/?"+canonical_querystring, headers=headers)
-        if r.status_code == 200:
-            return self.parse_xml_for_dirs(r.content)
-        else:
-            return None
 
     def build_credential_scope(self, date_stamp, region, service):
         return date_stamp + '/' + \
